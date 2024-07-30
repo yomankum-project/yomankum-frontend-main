1fa72019eddf5f8af6c92a8c6b8e5a1d2de8cf87
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2024-07-31 01:30:16 +0900

    shadcn setting

--- app/_components/main/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const Main = () => {
-  return <div className="font-bold-main">Main</div>
-}
---
+++ app/components/main/index.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { CustomButton } from "../custom"
+
+export const Main = () => {
+  return (
+    <div className="font-head1">
+      Main
+      <CustomButton className="font-par3">Buttons</CustomButton>
+    </div>
+  )
+}
